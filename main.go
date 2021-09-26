b46704b2af2311aa58319cb7309d7cb4e12e5e41
hz <hz@home>
2021-09-26 20:35:44 +0200

    fix(loop): ctrl+d forever loop

--- main.go
+++ main.go
@@ -4,14 +4,30 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
+
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigs
+		fmt.Println("\nBye bye \U0001F44B")
+		os.Exit(0)
+	}()
+
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
 		fmt.Print("> ")
-		scanner.Scan()
+		ok := scanner.Scan()
+		if !ok {
+			break
+		}
 		command := scanner.Text()
 		if len(command) == 0 {
 			continue
@@ -19,6 +35,9 @@ func main() {
 		tokens, err := Tokenize(command)
 		if err != nil {
 			fmt.Println(err)
+			continue
+		}
+		if len(tokens) == 0 {
 			continue
 		}
 
@@ -34,4 +53,5 @@ func main() {
 
 		Interpret(ast)
 	}
+
 }
