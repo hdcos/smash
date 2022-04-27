b423d002a90bbbd39c447cb5e10ac6559193ae1e
hz <hz@home.local>
2022-04-27 22:57:24 +0200

    feat(lever): add cmd token

--- java/src/main/java/io/smash/lexer/Lexems.java
+++ java/src/main/java/io/smash/lexer/Lexems.java
@@ -4,4 +4,10 @@ public class Lexems {
 
     public static final char AND_CHAR = '&';
     public static final String AND = "&&";
+
+    public static final char OR_CHAR = '|';
+    public static final String OR = "||";
+
+    public static final char PIPE_CHAR = '|';
+    public static final String PIPE = "|";
 }
--- java/src/main/java/io/smash/lexer/Lexer.java
+++ java/src/main/java/io/smash/lexer/Lexer.java
@@ -9,16 +9,25 @@ public class Lexer {
         return c == Lexems.AND_CHAR;
     }
 
+    public static boolean isOrLexem(char c) {
+        return c == Lexems.OR_CHAR;
+    }
+
     public static boolean isBlank(char c) {
         return c == ' ' || c == '\t';
     }
 
-    public static List<Token> tokenize(String line) {
+    public static boolean isAlphaNum(char c) {
+        return (c >= 'a' && c <= 'z')
+                || (c >= 'A' && c <= 'Z')
+                || c == '-'
+                || (c >= '0' && c <= '9');
+    }
+
+    public static List<Token> tokenize(String line) throws UnknownCharException {
         List<Token> tokens = new Vector<Token>();
 
         final int lineLength = line.length();
-
-        char previous = '\0';
 
         if (line != null && lineLength > 0) {
             int i = 0;
@@ -26,15 +35,44 @@ public class Lexer {
             while (i < lineLength) {
                 char c = line.charAt(i);
 
-                if (!isBlank(c)) {
-                    if (isAndLexem(c) && isAndLexem(previous)) {
-                        final Token and = new Token(Lexems.AND, 0, i);
+                if (isBlank(c)) {
+                    i++;
+                } else if (isAndLexem(c)) {
+                    if (i + 1 < lineLength && isAndLexem(line.charAt(i + 1))) {
+                        final Token and = new Token(TokenType.AND, 0, i, Lexems.AND);
                         tokens.add(and);
+                        i += 2;
+                    } else {
+                        throw new UnknownCharException(0, i, c);
                     }
+                } else if (isOrLexem(c)) {
+                    if (i + 1 < lineLength && isOrLexem(line.charAt(i + 1))) {
+                        final Token or = new Token(TokenType.OR, 0, i, Lexems.OR);
+                        tokens.add(or);
+                        i += 2;
+                    } else {
+                        final Token pipe = new Token(TokenType.PIPE, 0, i, Lexems.PIPE);
+                        tokens.add(pipe);
+                        i++;
+                    }
+                } else if (isAlphaNum(c)) {
+                    StringBuilder sb = new StringBuilder();
+                    int j = 0;
+                    char cc = c;
+                    while (isAlphaNum(cc)) {
+                        sb.append(cc);
+                        j++;
+                        if (i + j < lineLength) {
+                            cc = line.charAt(i + j);
+                        } else
+                            break;
+                    }
+                    final Token cmdPart = new Token(TokenType.CMD, 0, i, sb.subSequence(0, j));
+                    tokens.add(cmdPart);
+                    i += sb.length();
+                } else {
+                    throw new UnknownCharException(0, i, c);
                 }
-
-                previous = c;
-                i++;
             }
 
         }
